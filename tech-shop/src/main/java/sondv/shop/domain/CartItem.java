b3ae4146058ee9c7c1907f7e72fb3d7dfa96a84a
sowndv02 <daoson03112002@gmail.com>
2023-09-03 05:27:43 +0700

    create project

---
+++ tech-shop/src/main/java/sondv/shop/domain/CartItem.java
@@ -0,0 +1,15 @@
+package sondv.shop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+	private int productId;
+	private String name;
+	private int quantity;
+	private double unitPrice;
+}
---
+++ tech-shop/src/main/java/sondv/shop/domain/Category.java
@@ -0,0 +1,13 @@
+package sondv.shop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+	private int categoryId;
+	private String name;
+}
